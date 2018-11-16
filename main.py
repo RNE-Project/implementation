704ecbd46dd4606b143b7d4e748901dd8f8fbf70
Bogdan Cuza <bogdan.cuza@hotmail.com>
2018-11-16 17:22:18 +0900

    New features

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import sys
 from util import *
 import rumps
 from subprocess import Popen, PIPE
+from threading import Thread
+from os import listdir
+from os.path import isfile, join
+
 
 # parameters for loading data and images
 detection_model_path = 'face.xml'
@@ -27,76 +31,113 @@ emotion_classifier = load_model(emotion_model_path, compile=False)
 # getting input model shapes for inference
 emotion_target_size = emotion_classifier.input_shape[1:3]
 
-# starting lists for calculating modes
-emotion_window = []
-
 # starting video streaming
 video_capture = cv2.VideoCapture(0)
 
 #counter
 emotion_counter = [0] * 7
 
-def new_client(client, server):
-	conn = True
+server = WebsocketServer(13254, host='127.0.0.1', loglevel=logging.INFO)
 
-def client_left(client, server):
-    conn = False
+def websocket_thread(threadname):
+    global conn
+    global server
+    def new_client(client, server):
+        conn = True
+
+    def client_left(client, server):
+        conn = False	
+
+
+    server.set_fn_new_client(new_client)
+    server.set_fn_client_left(client_left)
+    server.run_forever()
 
 def notif(msg, title):
     cmd = "display notification \"{}\" with title \"{}\"".format(msg, title)
     #cmd = b"""display notification "Notification message" with title "Title" Subtitle "Subtitle" """
     Popen(["osascript", '-'], stdin=PIPE, stdout=PIPE).communicate(str.encode(cmd))
+    
+def pred_from_img(gray_image):
+    faces = detect_faces(face_detection, gray_image)
+    ar = []
 
-server = WebsocketServer(13254, host='127.0.0.1', loglevel=logging.INFO)
-server.set_fn_new_client(new_client)
-server.set_fn_client_left(client_left)
+    for face_coordinates in faces:
+
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+        gray_face = gray_image[y1:y2, x1:x2]
+        try:
+            gray_face = cv2.resize(gray_face, (emotion_target_size))
+        except:
+            continue
+        gray_face = preprocess_input(gray_face, False)
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+        ar.append(np.argmax(emotion_classifier.predict(gray_face)))
+
+    return ar
 
 
-def run_loop():
+def processing_thread(threadname):
+    global conn
+    global server
     while True:
         try:
 
             bgr_image = video_capture.read()[1]
             gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-            faces = detect_faces(face_detection, gray_image)
+            em = pred_from_img(gray_image)
+            
+            for e in em:
+                emotion_counter[e] += 1
 
-            for face_coordinates in faces:
-
-                x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-                gray_face = gray_image[y1:y2, x1:x2]
-                try:
-                    gray_face = cv2.resize(gray_face, (emotion_target_size))
-                except:
-                    continue
-                gray_face = preprocess_input(gray_face, False)
-                gray_face = np.expand_dims(gray_face, 0)
-                gray_face = np.expand_dims(gray_face, -1)
-                em = np.argmax(emotion_classifier.predict(gray_face))
-                emotion_counter[em] += 1
-                print("ok")
-
-                if conn != None:
+                if conn:
                     server.send_message_to_all("hi")
-
-                emotion_text = emotion_labels[em]
+    
+                emotion_text = emotion_labels[e]
         except:
             continue
 
 class App(rumps.App):
     def __init__(self):
         super(App, self).__init__("RNE")
-        self.menu = ["Start", "Say hi"]
+        self.menu = ["Check stats", "Order photos"]
+        thread1 = Thread( target=websocket_thread, args=("Thread-1", ) )
+        thread2 = Thread( target=processing_thread, args=("Thread-2", ) )
+        
+        thread1.join()
+        thread2.join()
 
-    @rumps.clicked("Say hi")
-    def sayhi(self, _):
-        notif("lol", "ok")
-
-    @rumps.clicked("Start")
-    def start(self, _):
-        run_loop()
+    @rumps.clicked("Check stats")
+    def check_stats(self, _):
+        s = sum(emotion_counter)
+        data = ""
+        for i in range(7):
+            emotion_counter[i]*100/s = percent
+            data += "{}: {} %\n".format(emotion_labels[i], int(percent))
+        notif(data, text)
+        
+    @rumps.clicked("Order photos")
+    def order(self, _):
+        path = ""
+        files = [f for f in listdir(path) if isfile(join(path, f))]
+        for i in range(7):
+            try:
+                os.makedirs(join(path, emotion_labels[i]))
+            except:
+                continue
+        for file in files:
+            img = cv2.imread(file, 0)
+            em = pred_from_img(img)
+            if em != []:
+                counter = [0]*7
+                for e in em:
+                    counter[0] += 1
+                category = counter.index(max(counter))
+                d = join(path, category)
+                os.rename(join(path, file), join(d, file))               
 
 App().run()
-server.run_forever()
 
 
 
