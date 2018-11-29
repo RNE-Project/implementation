8ffeda1c2ba59f9375f2a36529910eb4f28f79c8
Bogdan Cuza <bogdan.cuza@hotmail.com>
2018-11-29 17:00:54 +0900

    Add chart

--- ext/js/script.js
+++ ext/js/script.js
@@ -1,4 +1,6 @@
-function indexOfMax(arr) {
+//script.js
+
+/*function indexOfMax(arr) {
     if (arr.length === 0) {
         return -1;
     }
@@ -14,24 +16,43 @@ function indexOfMax(arr) {
     }
 
     return maxIndex;
-}
+}*/
 
 labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 
-em_txt = document.getElementById("emotion")
+domain = document.querySelector("#domain")
+var chart = new CanvasJS.Chart("chart", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	data: [{
+		type: "column",
+		showInLegend: true,
+		legendMarkerColor: "grey",
+	}]
+});
+
+
+
 chrome.tabs.getSelected(null, function(tab) {
 
     var url = new URL(tab.url)
     var domain = url.hostname
+    chart.title.text = domain
+
     chrome.storage.sync.get(['domains', 'emotion'], function(items) {
         domains = items.domains
         emotion = items.emotion
         if (domains != undefined) {
             i = domains.indexOf(domain)
             if (i != -1) {
-                em = indexOfMax(emotion[i])
-                em_txt.innerText = labels[em]
+                current_em = emotion[i]
+                dataPoints = []
+                for (var x = 0; x < 7; x++) {
+                    dataPoints.join({y: current_em[x], label: labels[x]})
+                }
+                chart.data[0].dataPoints = dataPoints
+                chart.render();
             }
         }
     });
